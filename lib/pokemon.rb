594fd86c748fd5efee45d4820c96de5464ea2e88
Amil Masic <amil.masic@gmail.com>
2019-03-24 10:50:30 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
 class Pokemon
-end
+  @@all = []
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id:, name:, type:, db:)
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  # def self.find (id, db)
+  #   @@all.each do |pokemon|
+  #     id = pokemon.id
+  #   end
+
+  end
+# end
